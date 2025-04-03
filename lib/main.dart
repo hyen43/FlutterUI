8b007e00a5c941fed201d81b31f00d61c0e5a08c
Nahyun Lee <lnhyen43@gmail.com>
2025-04-03 21:55:04 +0900

    design: ui design 작업중

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,92 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          elevation: 10,
-          backgroundColor: Color(0xFF000000),
-          title: Text("lina"),
-          // titleTextStyle: TextStyle(color: Color(0xFFFFFFFF)),
+        backgroundColor: Colors.black,
+
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 40),
+              (Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Hey,lina",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 34,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        "Welcome back",
+                        style: TextStyle(
+                          color: Colors.white.withAlpha(125),
+                          fontSize: 22,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )),
+              SizedBox(height: 40),
+              Text(
+                "Total Balance",
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+              SizedBox(height: 5),
+
+              Text(
+                "\$10,000",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 44,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(Radius.circular(45)),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 20,
+                      ),
+                      child: Text("Transfer", style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.all(Radius.circular(45)),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 20,
+                      ),
+                      child: Text(
+                        "Request",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        body: Center(child: Text("hello, world")),
       ),
     );
   }
